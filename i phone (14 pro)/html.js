2e6d15b2c5d933bcc1857d2daf6aa30361eb3cb6
سپهر سهرابی نژاد <129391741+sepehr-sorabi@users.noreply.github.com>
2023-04-10 04:15:54 -0700

    Add files via upload

---
+++ i phone (14 pro)/html.js
@@ -0,0 +1,32 @@
+// Code for opening apps when clicking on icons
+document.getElementById("app1").addEventListener("click", function () {
+    window.open("https://www.apple.com/");
+});
+
+document.getElementById("app2").addEventListener("click", function () {
+    window.open("https://www.icloud.com/");
+});
+
+document.getElementById("app3").addEventListener("click", function () {
+    window.open("https://www.apple.com/iphone/");
+});
+
+document.getElementById("app4").addEventListener("click", function () {
+    window.open("https://www.apple.com/watch/");
+});
+
+document.getElementById("app5").addEventListener("click", function () {
+    window.open("https://www.apple.com/ipad/");
+});
+
+document.getElementById("app6").addEventListener("click", function () {
+    window.open("https://www.apple.com/mac/");
+});
+
+document.getElementById("app7").addEventListener("click", function () {
+    window.open("https://www.apple.com/tv/");
+});
+
+document.getElementById("app8").addEventListener("click", function () {
+    window.open("https://www.apple.com/music/");
+});
